5b9dcc416bb4d685f34c7f5cf5c97e4cec8541f4
haibatcom123 <haibatcom123@gmail.com>
2021-05-23 02:17:39 +0700

    Success createValidation, create new create.blade.php
    test check validation input form in laravel

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,19 +9,26 @@ use App\Models\contract;
 class PostController extends Controller
 {
     public function showform() {
-        return view('test');
+        return view('create');
     }
     public function validationform(Request $request) {
-        echo "<pre>";
-            print_r($request->all());
-        
-        echo "</pre>";
         // $this->validation($request,[
         //     'name'=>'required|max:50',
         //     'address'=>'required',
         //     'email'=>'required',
         //     'content'=>'required'
         // ]);
+        echo "<pre>";
+            print_r($request->all());
+        
+        echo "</pre>";
+        $validatedData = $request->validate([
+            'name' => ['required', 'max:255', 'alpha'],
+            'address' => ['required'],
+            'email' => ['required', 'max:256', 'email'],
+            'content' => ['required'],
+        ]);
+        
     }
     public function addDatabase(Request $request){
         if($request->isMethod('post')){
@@ -30,12 +37,16 @@ class PostController extends Controller
         $email = $request->input("email");
         $content = $request->input("content");
 
-        // $contract = new contract([
-        //     'name'=>$name,
-        //     'address'=>$address,
-        //     'email'=>$email,
-        //     'content'=>$content
-        // ]);
+        echo "<pre>";
+            print_r($request->all());
+        
+        echo "</pre>";
+        $validatedData = $request->validate([
+            'name' => ['required', 'max:255'],
+            'address' => ['required'],
+            'email' => ['required', 'max:256', 'email'],
+            'content' => ['required'],
+        ]);
         $contract = new contract();
 
         $contract->name = $name;
@@ -44,11 +55,7 @@ class PostController extends Controller
         $contract->content = $content;
             
         $contract->save();
-        echo "<pre>";
-            print_r($request->all());
         
-        echo "</pre>";
-        }
-
     }
+}
 }
--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,14 +1,28 @@
-<!doctype html>
 <html>
-<head>
-    <title>Login Form</title>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="public/css/style.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Charm|Montserrat" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+        <link rel="stylesheet" href="./public/css/login.css"/>
+        <title>Result</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+                    <div class="card card-signin my-5">
+                        <div class="card-body">
+
+                            <head>
+    <h1>Creat a new contract</h1>
     <style type="text/css">
         .error-message { color: red; }
     </style>
 </head>
-
 <body>
-    @if (count($errors) > 0)
+@if (count($errors) > 0)
     <div class="error-message">
         <ul>
             @foreach ($errors->all() as $error)
@@ -17,19 +31,32 @@
         </ul>
     </div>
     @endif
+<form action="{{ route('create') }}" method="post" >
+@csrf
+    <div class="form-label-group">
+        <label for="inputName">Name</label>
+        <input type="text" id="name" class="form-control" name="name" placeholder="e.g. Hoang Duc Truong" autofocus>                                    
+    </div>
+    <div class="form-label-group">
+        <label for="inputAddress">Address</label>
+        <input type="text" id="address" class="form-control" name="address" placeholder="Where do you live?" >             
+    </div>
+    <div class="form-label-group">
+        <label for="inputEmail">Email</label>
+        <input type="text" id="email" class="form-control" name="email" placeholder="e.g. haibatcom123@gmail.com" >                                    
+    </div>
+    <div class="form-label-group">
+        <label for="inputContent">Content</label>
+        <input type="text" id="content" class="form-control" name="content" placeholder="You must input somehing here!" >             
+    </div>
+    <br>
+        <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit" value="create" name="create">Create</button>
 
-    <form action="{{router('create')}}" method="post">
-        @csrf
-        <p>Name<br>
-            <input type="text" name="name" value=""></p>
-        <p>Address<br>
-            <input type="text" name="address" value=""></p>
-        <p>Email<br>
-            <input type="text" name="email" value=""></p>
-        <p>Content<br>
-            <input type="text" name="content" value=""></p>
-
-        <p><button type="submit">Submit</button></p>
-    </form>
+</form>
 </body>
-</html> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</html>
